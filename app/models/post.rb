9203a32e826c501f602b68cb1281bcda79d1d450
thomas dubiel <github email address>
2020-05-19 16:01:03 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,23 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  #accepts_nested_attributes_for :categories
+  #use above line only if creating a new category every time
+  #instead we will define custom category writee
+
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      category = Category.find_or_create_by(category_attribute)
+      self.categories << category if category.name != ""
+    end
+  end
+
+  def uniq_users
+    uniq_commenters = []
+    self.comments.each do |comment|
+      uniq_commenters << comment.user if !uniq_commenters.include?(comment.user)
+    end
+    uniq_commenters
+  end
 
 end
